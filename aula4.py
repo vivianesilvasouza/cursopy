93b062cef3cdecc61bf7db3b5fec8932f166fbde
viviane souza <vivianedasilvasouza2020@gmail.com>
2020-12-13 17:12:30 -0300

    first commit

---
+++ aula4.py
@@ -0,0 +1,24 @@
+print(type('luiz'))  ###o type ele mostra que tipo de dados é
+
+print(type(10))  #não se coloca aspa em números, pois sera entendido como uma strig
+
+print(type(10 == 10))
+
+print(type(10.25))
+
+print('l' == 'k', type('L' == 'l'))
+
+print('luiz', type('luiz'), bool('luiz'))
+
+#string: nome
+print('viviane', type('viviane'))
+
+#idade:int
+print(10, type(10))
+
+#altura:float
+print(1.58, type(1.58))
+
+#é maior de idade 10>20
+print(27 > 18, type(27 > 18))
+
